c3b25b4189c31fbc6ebf7f8f91be72ecbd6c0166
Navdish <navdishjaggi@gmail.com>
2024-02-15 18:37:55 +0530

    15g

--- client/src/Components/Signup/signup.jsx
+++ client/src/Components/Signup/signup.jsx
@@ -1,7 +1,19 @@
-import react from 'react'
+import * as React from 'react';
 import {useState} from 'react'
 import './signup.css'
 import axios from 'axios'
+import Box from '@mui/system/Box';
+import logo from"../../Assets/images/Logo.png"
+import InputLabel from '@mui/material/InputLabel';
+import Input from '@mui/material/Input';
+import { Link } from '@mui/material';
+import FormControl from '@mui/material/FormControl';
+import InputAdornment from '@mui/material/InputAdornment';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
 
 function Signup(){
     const [name, setName] = useState("");
@@ -13,15 +25,35 @@ function Signup(){
         console.log(response);
     }
     return (
-        <div className='signup'>
-            <h1>Signup Form</h1>
+        <Box className='outside'>
+            {/* <h1>Signup Form</h1>
             <form className='signup-form'>
                 <input type="text" placeholder='name' value={name} onChange={(e)=> setName(e.target.value)}/>
                 <input type="email" placeholder="email" value={email} onChange={(e)=> setEmail(e.target.value)}/>
                 <input type="password" placeholder="password" value={password} onChange={(e)=> setPassword(e.target.value)}/>
                 <button type="submit" onClick={(e)=> handleSubmit(e)}>Submit</button>
-            </form>
-        </div>
+            </form> */}
+            
+            <Box className='outside2'>
+                <Box className='logo-Box'><img src={logo} alt='' className='logo-img'/></Box>
+                <Box className='title'>Make the most of your professional life</Box>
+                <Box className='signup'>
+                    <Box className='complete-input'>
+                        <InputLabel htmlFor="my-input" className='form-input-label' sx={{ fontStyle:"normal", fontWeight: "600", lineHeight: "20px", fontSize:"14px", color:"rgb(0,0,0,0.75)"}}>Email or phone number</InputLabel>
+                        <Input id="my-input" aria-describedby="my-helper-text" sx={{ width:"100%"}} className='form-input' />
+                    </Box>
+                    <Box className='complete-input'>
+                        <InputLabel htmlFor="my-input" className='form-input-label' sx={{ fontStyle:"normal", fontWeight: "600", lineHeight: "20px", fontSize:"14px", color:"rgb(0,0,0,0.75)"}}>Password (6+ characters)</InputLabel>
+                        <Input id="my-input" aria-describedby="my-helper-text" sx={{ width:"100%"}} className='form-input' />
+                    </Box>
+                    <Box>
+                        
+                    </Box>
+                </Box>
+                <Box className="after-signup">Looking to create a page for a business? <Link>Get Help</Link></Box>
+            </Box>
+
+        </Box>
     );
 }
 
